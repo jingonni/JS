8111e4a92117a3410303b2551630c60de4f4bc67
jingonni <gojin970930@naver.com>
2023-11-27 14:44:20 +0900

    12-24 고침

--- timer.js
+++ timer.js
@@ -2,30 +2,32 @@ const clock = document.getElementById("clock");
 const clockWatch = document.getElementById("clockWatch");
 
 function getClock() {
-  //console.log(clockWatch.value);
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  //if(clockWatch.value == 24){
+  if(clockWatch.value == 24){
   clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
-  //}else{
-   /* if(hours >= 12) { 
-      const hour = hours-12;
-      console.log(typeof hours)
-      console.log(hour);
-      clock.innerText = `${hour < 10 ? `0${hour}` : hour}:${
+  }else{
+    if (clockWatch.value == 24) {
+      clock.innerText = `AM ${hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes
       }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    } else {
+      let time_h = hours;
+      if (hours >= 12) {
+        time_h = hours - 12;
+      }
+      
+      clock.innerText = ` PM ${time_h < 10 ? `0${time_h}` : time_h}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    }
   }
-     
-    clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-      minutes < 10 ? `0${minutes}` : minutes
-    }:${seconds < 10 ? `0${seconds}` : seconds}`;
-  }*/
-}
+  }
+
 
 
 
